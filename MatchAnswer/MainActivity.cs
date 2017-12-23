57e175cc621a20a739a5c412cd6e8c4c755b0092
geneLocated <genelocated@foxmail.com>
2017-12-23 21:00:39 +0800

    Add FindInAns

--- MatchAnswer/MainActivity.cs
+++ MatchAnswer/MainActivity.cs
@@ -27,8 +27,12 @@ namespace MatchAnswer
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
             GetQA();
-            FindInQuest("实施乡村振兴战略。____问题是关");
+             string answer = FindInAns(FindInQuest("实施乡村振兴战略。____问题是关"));
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this)
+                .SetMessage(answer);
+            alertDialog.Show();
         }
 
         private List<string> GetFromAssets(string fileName)
@@ -52,14 +56,29 @@ namespace MatchAnswer
         {
             string result = null;
             foreach (string currLine in strQuest)
-                if (currLine.Contains(target))
+                if (currLine.Contains(target))  //若该行包含target
                 {
                     int stopIndex;
                     if (currLine.Contains("．")) //若存在"．"
-                        stopIndex = currLine.LastIndexOf("．");
+                        stopIndex = currLine.IndexOf("．");
                     else
-                        stopIndex = currLine.LastIndexOf(".");
+                        stopIndex = currLine.IndexOf(".");
                     result = currLine.Substring(0, stopIndex);
+                }
+            return result;
+        }
+
+        private string FindInAns(string target) //返回包含该字符串（序号）的答案内容
+        {
+            string result = null;
+            foreach (string currLine in strAns)
+                if (currLine.Contains(target))  //若该行包含target
+                {
+                    int targetIndex = currLine.IndexOf(target);
+                    int startIndex = currLine.IndexOf(".", targetIndex) + 1; ////从targetIndex后开始查找"."
+                    //加一，因为点后面才是答案内容
+                    int stopIndex = currLine.IndexOf(" ", startIndex + 1);  //从答案内容开始后查找空格
+                    result = currLine.Substring(startIndex, stopIndex - startIndex);
                 }
             return result;
         }
