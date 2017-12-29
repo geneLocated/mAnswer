fadd1f8595b2808a1649435cfbaa0b553b08e1f3
geneLocated <genelocated@foxmail.com>
2017-12-29 22:28:29 +0800

    设计窗体

--- 匹配答案/Form1.Designer.cs
+++ 匹配答案/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 41);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(634, 720);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://jyj.hizh.cn/index.php?s=/addon/AnswerDraw/Wapapi/PCzhjy.html", System.UriKind.Absolute);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(547, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "开始匹配";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "http://jyj.hizh.cn/index.php?s=/addon/AnswerDraw/Wapapi/PCzhjy.html";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(634, 761);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "匹配答案";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
